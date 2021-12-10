aa7f35e2dccad8efdbe95cc0f4360bc7858cc288
klauth86 <kehel@yandex.ru>
2021-12-10 18:33:19 +0500

    Initial commit

---
+++ FPSExample/Source/FPSExample/FPSExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSEXAMPLE_API AFPSExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
