229a9236327205feddc9bdaa96a4c6810d1fb0de
klauth86 <kehel@yandex.ru>
2021-12-11 13:49:05 +0500

    AI p1

--- FPSExample/Source/FPSExample/AI/BTService_Analyze.cpp
+++ FPSExample/Source/FPSExample/AI/BTService_Analyze.cpp
@@ -1,4 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AI/BTService_Analyze.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Characters/BasicCharacter.h"
 
+void UBTService_Analyze::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	if (UBlackboardComponent* bb = OwnerComp.GetBlackboardComponent())
+	{
+		if (ABasicCharacter* self = Cast<ABasicCharacter>(bb->GetValueAsObject(BK_SelfActor.SelectedKeyName)))
+		{
+
+		}
+	}
+}
--- FPSExample/Source/FPSExample/AI/BTService_Analyze.h
+++ FPSExample/Source/FPSExample/AI/BTService_Analyze.h
@@ -2,16 +2,26 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "BehaviorTree/BTService.h"
 #include "BTService_Analyze.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class FPSEXAMPLE_API UBTService_Analyze : public UBTService
 {
 	GENERATED_BODY()
 	
+protected:
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		struct FBlackboardKeySelector BK_SelfActor;
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		struct FBlackboardKeySelector BK_Target;
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		struct FBlackboardKeySelector BK_State;
 };
