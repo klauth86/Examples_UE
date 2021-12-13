04b93ae4bcdf411cf3e64ba54b1ab8f396f19697
klauth86 <kehel@yandex.ru>
2021-12-13 14:05:34 +0500

    AI p19 BehTree

---
+++ FPSExample/Source/FPSExample/EQ/EQContext_Base.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "EQContext_Base.generated.h"
+
+UCLASS()
+class FPSEXAMPLE_API UEQContext_Base : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+};
---
+++ FPSExample/Source/FPSExample/EQ/Filter/EQTest_TargetIsInSight.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "EQTest_TargetIsInSight.generated.h"
+
+UCLASS()
+class FPSEXAMPLE_API UEQTest_TargetIsInSight : public UEnvQueryTest
+{
+	GENERATED_BODY()
+	
+};
