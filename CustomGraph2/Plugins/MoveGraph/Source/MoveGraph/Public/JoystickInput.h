2fed89acc40ffa4ae449076d2a8e23a65a64b382
klauth86 <kehel@yandex.ru>
2021-12-15 20:34:24 +0500

    MoveEditor p8

--- CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/JoystickInput.h
+++ CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/JoystickInput.h
@@ -12,5 +12,69 @@ struct MOVEGRAPH_API FJoystickInput
 
 public:
 
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_0 : 1;
 
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_45 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_90 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_135 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_180 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_225 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_270 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_315 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftStick_315 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftTrigger : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 LeftUpperTrigger : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_0 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_45 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_90 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_135 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_180 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_225 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_270 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_315 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightStick_315 : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightTrigger : 1;
+
+	UPROPERTY(EditAnywhere)
+		uint32 RightUpperTrigger : 1;
 };
