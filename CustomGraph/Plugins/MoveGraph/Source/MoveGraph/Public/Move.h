e7a4f0e1654a2713ae5ab062b8a741cd46e546f8
klauth86 <kehel@yandex.ru>
2021-12-15 18:34:27 +0500

    MoveEditor p8

--- CustomGraph/Plugins/MoveGraph/Source/MoveGraph/Public/Move.h
+++ CustomGraph/Plugins/MoveGraph/Source/MoveGraph/Public/Move.h
@@ -22,40 +22,5 @@ class MOVEGRAPH_API UMove : public UObject
 
 public:
 
-#if WITH_EDITORONLY_DATA
-	UPROPERTY()
-		TArray<UMoveNode*> AllNodes;
 
-	UPROPERTY()
-		UEdGraph* MoveGraph;
-#endif
-
-#if WITH_EDITOR
-	/** Create the basic sound graph */
-	void CreateGraph();
-
-	/** Clears all nodes from the graph (for old editor's buffer soundcue) */
-	void ClearGraph();
-
-	/** Set up EdGraph parts of a SoundNode */
-	void SetupSoundNode(UMoveNode* InSoundNode, bool bSelectNewNode = true);
-
-	/** Use the SoundCue's children to link EdGraph Nodes together */
-	void LinkGraphNodesFromSoundNodes();
-
-	/** Use the EdGraph representation to compile the SoundCue */
-	void CompileSoundNodesFromGraphNodes();
-
-	/** Get the EdGraph of SoundNodes */
-	UEdGraph* GetGraph();
-
-	/** Resets all graph data and nodes */
-	void ResetGraph();
-
-private:
-
-	/** Recursively sets the branch culling exclusion on random nodes in this sound cue. */
-	void RecursivelySetExcludeBranchCulling(UMoveNode* CurrentNode);
-
-#endif // WITH_EDITOR
 };
