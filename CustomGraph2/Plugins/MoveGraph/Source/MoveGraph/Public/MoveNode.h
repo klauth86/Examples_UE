c1c297d442827a2bcfc724ac3997597bb9601b46
klauth86 <kehel@yandex.ru>
2021-12-15 19:53:01 +0500

    MoveEditor p4

---
+++ CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/MoveNode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MoveNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEGRAPH_API UMoveNode : public UObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/Node.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Node.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVEGRAPH_API UNode : public UObject
+{
+	GENERATED_BODY()
+	
+};
