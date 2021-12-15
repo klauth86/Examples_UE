2ef3ff196f395032a64479699486388b58295c84
klauth86 <kehel@yandex.ru>
2021-12-15 14:42:27 +0500

    Add CustomGraph proj

---
+++ CustomGraph/Source/CustomGraph/CustomGraphGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomGraphGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMGRAPH_API ACustomGraphGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
