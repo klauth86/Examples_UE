5c9eed65e1544f54d6057c339c7374c72da2a6d6
klauth86 <kehel@yandex.ru>
2021-12-15 20:50:00 +0500

    MoveEditor p9

--- CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/JoystickInput.h
+++ CustomGraph2/Plugins/MoveGraph/Source/MoveGraph/Public/JoystickInput.h
@@ -12,69 +12,118 @@ struct MOVEGRAPH_API FJoystickInput
 
 public:
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_0 : 1;
+	FJoystickInput()
+	{
+		PackedBits = 0;
+	}
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_45 : 1;
+	union
+	{
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_90 : 1;
+		struct
+		{
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_135 : 1;
+			// LEFT STICK
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_180 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_0 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_225 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_45 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_270 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_90 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_315 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_135 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftStick_315 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_180 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftTrigger : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_225 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 LeftUpperTrigger : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_270 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_0 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_315 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_45 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftStick_HOLD : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_90 : 1;
+			// LEFT TRIGGER
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_135 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftTrigger : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_180 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 LeftUpperTrigger : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_225 : 1;
+			// RIGHT STICK
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_270 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_0 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_315 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_45 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightStick_315 : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_90 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightTrigger : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_135 : 1;
 
-	UPROPERTY(EditAnywhere)
-		uint32 RightUpperTrigger : 1;
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_180 : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_225 : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_270 : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_315 : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightStick_HOLD : 1;
+
+			// RIGHT TRIGGER
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightTrigger : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 RightUpperTrigger : 1;
+
+			// XYAB
+
+			UPROPERTY(EditAnywhere)
+				uint64 X : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 X_HOLD : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 Y : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 Y_HOLD : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 A : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 A_HOLD : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 B : 1;
+
+			UPROPERTY(EditAnywhere)
+				uint64 B_HOLD : 1;
+		};
+
+		uint64 PackedBits;
+	};
 };
