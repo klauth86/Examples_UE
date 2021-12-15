11eb8de35d0b672d972c5455d030ec20fe8f3bfd
klauth86 <kehel@yandex.ru>
2021-12-15 20:25:52 +0500

    MoveEditor p6

---
+++ CustomGraph2/Source/CustomGraph2/JoystickInput.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "UObject/NoExportTypes.h"
+#include "JoystickInput.generated.h"
+
+USTRUCT(BlueprintType)
+struct CUSTOMGRAPH2_API FJoystickInput
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+
+};
