f0a3c4bc8a254f94820abaa466d5ffa28e178b08
klauth86 <kehel@yandex.ru>
2021-12-17 09:33:17 +0500

    Street tycoon p9

--- StreetTycoon/Source/StreetTycoon/StreetTycoonGameModeBase.cpp
+++ StreetTycoon/Source/StreetTycoon/StreetTycoonGameModeBase.cpp
@@ -2,7 +2,9 @@
 
 #include "StreetTycoonGameModeBase.h"
 #include "StreetTycoonPlayerController.h"
+#include "StreetTycoonPawn.h"
 
 AStreetTycoonGameModeBase::AStreetTycoonGameModeBase() {
 	PlayerControllerClass = AStreetTycoonPlayerController::StaticClass();
+	DefaultPawnClass = AStreetTycoonPawn::StaticClass();
 }
---
+++ StreetTycoon/Source/StreetTycoon/StreetTycoonPawn.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "StreetTycoonPawn.generated.h"
+
+UCLASS()
+class STREETTYCOON_API AStreetTycoonPawn : public APawn
+{
+	GENERATED_BODY()
+};
